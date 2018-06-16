f8f8ec164aac993127cc0a73f709fba6774d85a3
johnnyd77 <nicolas.arayac27@gmail.com>
2018-06-16 09:38:28 -0400

    actualizacion

--- db/migrate/20180615194428_add_name.rb
+++
@@ -1,5 +0,0 @@
-class AddName < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :pins, :name , :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_194428) do
+ActiveRecord::Schema.define(version: 2018_06_16_132906) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
